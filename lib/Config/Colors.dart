a1c2370aa5afecac0698cf1fd83d395aefc5315b
MrNitishroy <mr.nitishkrs@gmail.com>
2023-09-22 13:09:53 +0530

    Welcome page completed

---
+++ lib/Config/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF0057FF);
+const secondryColor = Color(0xFF00C236);
+const backgroudColor = Color(0xFFFFFFFF);
+const fontColor = Color(0xFF000000);
+const lebelColor = Color(0xFF6B6B6B);
